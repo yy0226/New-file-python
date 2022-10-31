3f2f7b04fef2a6028c2da5c1ee0d6e93b1d24cad
Lu Ye <yeruolu@uw.edu>
2022-10-31 16:45:58 -0700

    changed yvonne's

--- Week 4_Lab.py
+++ Week 4_Lab.py
@@ -7,7 +7,7 @@ board = [
     "......................",
     "......##########......",
     "......#........#......",
-    "......#........#......",
+    "......#....#...#......",
     "......#........#####..",
     "....###............#..",
     "....#............###..",
@@ -30,9 +30,9 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     len_x = len(input_board)
     len_y = len(input_board[0])
     if x < 0 or x >= len_x:
-        return
+        return input_board
     if y < 0 or y >= len_y:
-        return
+        return input_board
     if input_board[x][y] == "#" or input_board[x][y] == new: return
     if input_board[x][y] == ".":
         input_board[x]=input_board[x][0:y]+ new +input_board[x][y+1:]
